f23cb4f4e16ca3bb5cb11b10e0fc3f090d4691f6
Ryan Azrian <azrian.ryan@gmail.com>
2018-04-21 00:10:51 +0700

    Ryan Regex and his friends

--- src/pages/addrepair/addrepair.ts
+++ src/pages/addrepair/addrepair.ts
@@ -56,6 +56,7 @@ export class AddrepairPage {
   percentageLoaded: any
 
 
+
   lat : string;
   lang : string;
   token : any;
@@ -230,20 +231,40 @@ export class AddrepairPage {
     if(form.valid){
       loading.present();
 
-      let kelengkapan = 'charger = '+this.charger + ',' +  
-                         'mouse = '+ this.mouse + ',' + 
-                         'mushrooms = '+ this.mushrooms + ',' +
-                         'cd = '+this.cd;
-      // console.log(this.tipeKerusakan)
-      // if(this.service != "a"){
-      //   this.tipeKerusakan = JSON.stringify(this.service);
-      //   this.tipeKerusakan = this.tipeKerusakan.replace('[', '');
-      //   this.tipeKerusakan = this.tipeKerusakan.replace(']', ''); 
-      //   for(var i= 0 ; i< this.service.length*2;i++)
-      //   this.tipeKerusakan = this.tipeKerusakan.replace('"', '');
-      // }
+      var kelengkapans = [];
+      if(this.charger){
+        var chargers = "chargers";
+        kelengkapans[kelengkapans.length] = chargers;
+      }
+      if(this.mouse){
+        var mouses = "mouse";
+        kelengkapans[kelengkapans.length] = mouses;
+      }
+      if(this.mushrooms){
+        var mushroomss = "Tas";
+        kelengkapans[kelengkapans.length] = mushroomss;
+      }
+      if(this.cd){
+        var cds = "cd";
+        kelengkapans[kelengkapans.length] = cds;
+      }
 
-      console.log(this.tipeKerusakan);
+
+
+      // let kelengkapan = chargers + ',' + mouses + ',' + mushroomss + ',' +cds;
+      // console.log(this.tipeKerusakan)
+      if(kelengkapans.length != 0){
+        var kelengkapan = JSON.stringify(kelengkapans);
+        kelengkapan = kelengkapan.replace('[', '');
+        kelengkapan = kelengkapan.replace(']', ''); 
+        for(var i= 0 ; i< kelengkapans.length*2;i++)
+        kelengkapan = kelengkapan.replace('"', '');
+      }
+      else{
+        kelengkapan = "Tidak ada barang";
+      }
+
+      console.log(kelengkapan);
 
       let input = JSON.stringify({
         user_id : this.id, 
